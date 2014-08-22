adec1505a9be899e8d5b57244980e9a38f95cf32
Emanuele <feliziani.emanuele@gmail.com>
2014-08-22 19:27:38 +0200

    Implement AJAX location sending
    Location data are sent via AJAX and it's working nicely.
    
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- public/geo.js
+++ public/geo.js
@@ -1,30 +1,44 @@
 window.addEventListener('load', function() {
 	
-	function getMyPosition(position) {
+	function sendIt( thingToSend ) {
+		var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+		
+		// We define what will happen if the data are successfully sent
+		xmlhttp.addEventListener('load', function(event) {
+			document.write( event.target.responseText );
+		});
+		
+		xmlhttp.open('POST', '/save-position');
+		xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+		xmlhttp.send( thingToSend );
+	}
+	
+	function sendMyPosition(position) {
 //		document.write(position.coords.longitude + ' | ' + position.coords.latitude + ' | accuracy= ' + position.coords.accuracy + '<br>');
 		var ratto = {
 			lon: position.coords.longitude,
-			lat: position.coords.latitude,
-			acc: position.coords.accuracy,
-			alt: position.coords.altitude,
-			spe: position.coords.speed
+			lat: position.coords.latitude
 		};
-		document.write( JSON.stringify(ratto) + '<br>' );
+		ratto = JSON.stringify( ratto );
+		sendIt( ratto );
 	};
 	
 	function error(err) {
-	  console.warn('ERROR(' + err.code + '): ' + err.message);
+		console.warn('ERROR(' + err.code + '): ' + err.message);
 	};
 	
 	options = {
-	  enableHighAccuracy: true,
-	  timeout: 27000,
-	  maximumAge: 30000
+		enableHighAccuracy: true,
+		timeout: 27000,
+		maximumAge: 30000
 	};
 	
 	if ( 'geolocation' in navigator ) {
-		navigator.geolocation.watchPosition(getMyPosition, error, options);
+		navigator.geolocation.getCurrentPosition(sendMyPosition, error, options);
 	} else {
 		alert('Get rid of your fucking browser, you demented bastard!');
 	}
+	
+	
+	
 });
